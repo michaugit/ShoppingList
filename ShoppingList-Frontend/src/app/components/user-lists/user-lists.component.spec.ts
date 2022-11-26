227ed351786c1b8bab24a6c743de0a5eb2f0a775
Michał Pieniądz <mpieniadz@student.agh.edu.pl>
2022-11-26 21:49:28 +0100

    update tests

--- ShoppingList-Frontend/src/app/components/user-lists/user-lists.component.spec.ts
+++ ShoppingList-Frontend/src/app/components/user-lists/user-lists.component.spec.ts
@@ -1,16 +1,12 @@
 import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import { UserListsComponent } from './user-lists.component';
-import {ItemService} from "../../services/item.service";
-import {ListResponse} from "../../models/responses/listResponse";
-import UnitService from "../../services/unitService";
 import * as Rx from "rxjs";
 import {throwError} from "rxjs";
 import Swal from "sweetalert2";
 import {HttpTestingController} from "@angular/common/http/testing";
 import {commonTestImports, materialTestImports} from "../../app.providers";
 import {AddUserListComponent} from "./add-user-list/add-user-list.component";
-import {EditUserListComponent} from "./edit-user-list/edit-user-list.component";
 import {List} from "../../models/list";
 import {StorageService} from "../../services/auth/storage.service";
 import {Router} from "@angular/router";
@@ -18,6 +14,8 @@ import {UserListComponent} from "./user-list/user-list.component";
 import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from "@angular/material/core";
 import {MomentDateAdapter} from "@angular/material-moment-adapter";
 import {CUSTOM_DATE_FORMATS} from "../../models/custom-date-formats";
+import {UserListsResponse} from "../../models/responses/userListsResponse";
+import {UserListsService} from "../../services/user-lists.service";
 
 describe('UserListsComponent', () => {
   let component: UserListsComponent;
@@ -53,54 +51,60 @@ describe('UserListsComponent', () => {
     component.ngOnInit()
     expect(spyRouter).toHaveBeenCalledWith(['/login'])
   });
-  //
-  // it('should contain add-list-item-component', () => {
-  //   const addListItemComponent = fixture.debugElement.nativeElement.querySelector('app-add-list-item')
-  //   expect(addListItemComponent).toBeDefined()
-  // });
-  //
-  // it('should contain list-item-component when number of items more than zero', () => {
-  //   expect(component.items.length).toBeGreaterThan(0)
-  //   const listItemComponent = fixture.debugElement.nativeElement.querySelector('app-list-item')
-  //   expect(listItemComponent).toBeDefined()
-  // });
-  //
-  // it('should not contain list-item-component when number of items equals zero', () => {
-  //   component.items = []
-  //   fixture.detectChanges()
-  //   expect(component.items.length).toBe(0)
-  //   const listItemComponent = fixture.debugElement.nativeElement.querySelector('app-list-item')
-  //   expect(listItemComponent).toBeNull()
-  // });
-  //
-  // it('items should be refreshed when request successful', fakeAsync( () => {
-  //   const itemService = fixture.debugElement.injector.get(ItemService)
-  //   const response: ListResponse={
-  //     "listId": component.listId,
-  //     "listName": "testName",
-  //     "date" : "2022-11-26",
-  //     "items": [{'id': 1, 'text': 'any product', 'quantity': 2, 'unit': UnitService.getDefaultUnit()[1], 'done': false, "image": null},
-  //       {'id': 2, 'text': 'any product', 'quantity': 2, 'unit': UnitService.getDefaultUnit()[1], 'done': false, "image": null}]}
-  //   spyOn(itemService, 'getListItems').and.returnValue(Rx.of(response))
-  //   component.refreshItems()
-  //   expect(component.items.length).toBe(2)
-  // }));
-  //
-  // it('should display sweetAlert when request error', fakeAsync( () => {
-  //   const itemService = fixture.debugElement.injector.get(ItemService)
-  //   const mockErrorResponse = { status: 401, statusText:"Unauthorized", error: { message: 'Smth went wrong' } };
-  //   spyOn(itemService, 'getListItems').and.returnValue(throwError(() => mockErrorResponse))
-  //   const spySweetAlert = spyOn(Swal, "fire")
-  //   component.refreshItems()
-  //   tick(1000)
-  //
-  //   expect(component.items.length).toBe(1)
-  //   expect(spySweetAlert).toHaveBeenCalled()
-  // }));
-  //
-  // it( 'removeItem() should remove items from components list', fakeAsync( () => {
-  //   const itemToRemove = component.items[0]
-  //   component.removeItem(itemToRemove)
-  //   expect(component.items.find(function (listItem){return listItem.id == itemToRemove.id})).toBeUndefined()
-  // }));
+
+  it('should fetch lists if user is logged in', () => {
+    const storageService = fixture.debugElement.injector.get(StorageService)
+    spyOn(storageService, 'isLoggedIn').and.returnValue(true)
+    const spyRefreshLists = spyOn(component, 'refreshUserLists')
+    component.ngOnInit()
+    expect(spyRefreshLists).toHaveBeenCalled()
+  });
+
+  it('should contain add-user-list-component', () => {
+    const addListItemComponent = fixture.debugElement.nativeElement.querySelector('app-user-list')
+    expect(addListItemComponent).toBeDefined()
+  });
+
+  it('should contain user-list-component when number of items more than zero', () => {
+    expect(component.lists.length).toBeGreaterThan(0)
+    const listItemComponent = fixture.debugElement.nativeElement.querySelector('app-user-list')
+    expect(listItemComponent).toBeDefined()
+  });
+
+  it('should not contain user-list-component when number of items equals zero', () => {
+    component.lists = []
+    fixture.detectChanges()
+    expect(component.lists.length).toBe(0)
+    const listItemComponent = fixture.debugElement.nativeElement.querySelector('app-user-list')
+    expect(listItemComponent).toBeNull()
+  });
+
+  it('items should be refreshed when request successful', fakeAsync( () => {
+    const listService = fixture.debugElement.injector.get(UserListsService)
+    const response: UserListsResponse={
+      "shoppingLists": [
+        {"id": 1, "name": "test name", "date": "2022-11-26"},
+        {"id": 2, "name": "test name2", "date": "2022-11-27"}]}
+    spyOn(listService, 'getUserLists').and.returnValue(Rx.of(response))
+    component.refreshUserLists()
+    expect(component.lists.length).toBe(2)
+  }));
+
+  it('should display sweetAlert when request error', fakeAsync( () => {
+    const listService = fixture.debugElement.injector.get(UserListsService)
+    const mockErrorResponse = { status: 401, statusText:"Unauthorized", error: { message: 'Smth went wrong' } };
+    spyOn(listService, 'getUserLists').and.returnValue(throwError(() => mockErrorResponse))
+    const spySweetAlert = spyOn(Swal, "fire")
+    component.refreshUserLists()
+    tick(1000)
+
+    expect(component.lists.length).toBe(1)
+    expect(spySweetAlert).toHaveBeenCalled()
+  }));
+
+  it( 'removeList() should remove list from components list', fakeAsync( () => {
+    const listToRemove = component.lists[0]
+    component.removeList(listToRemove)
+    expect(component.lists.find(function (list){return list.id == listToRemove.id})).toBeUndefined()
+  }));
 });
