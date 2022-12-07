14319f59d906d9da9c6e7c43d53c8475cd731d18
Michał Pieniądz <mpieniadz@student.agh.edu.pl>
2022-12-07 20:27:49 +0100

    remove unused import

--- ShoppingList-Backend/src/main/java/com/agh/shoppingListBackend/app/controllers/ItemController.java
+++ ShoppingList-Backend/src/main/java/com/agh/shoppingListBackend/app/controllers/ItemController.java
@@ -18,12 +18,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 import java.io.IOException;
 
 @RestController
